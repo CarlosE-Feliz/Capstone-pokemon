8aa9476583ce0d0b9f81339664f308cabf70da39
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-09-07 11:44:05 -0600

    add card, change background, add stlyes and change API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+// Bulbasaur, chamander, squirtle, pikachu, diglett, tentacruel
+const API_KEY = 'https://pokeapi.co/api/v2/pokemon?limit=6';
+
+const poke = async () => {
+  await fetch(API_KEY, {
+    method: 'GET',
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
+
+poke();
