4210d76bc3609e2e1f77a915be2ae87f8e271cd7
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-09-07 16:53:55 -0600

    add functionality to display info

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import './styles.css';
-// Bulbasaur, chamander, squirtle, pikachu, diglett, tentacruel
-const API_KEY = 'https://pokeapi.co/api/v2/pokemon?limit=6';
 
-const poke = async () => {
-  await fetch(API_KEY, {
-    method: 'GET',
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-};
+const divDad = document.querySelector('.row1');
+function creaPoke(pokemon) {
+  const newValues = pokemon;
+  for (let i = 0; i < newValues.length; i += 1) {
+    divDad.innerHTML += '<div class="col-4" >'
+    + '<div class="card">'
+      + `<img src="${newValues[i].sprites.front_default}" class="card-img-top" alt="image" />`
+       + '<div class="card-body">'
+          + `<h5 class="card-title">${newValues[i].name}</h5>`
+          + '<p class="card-text"></p>'
+          + `<button class="btn btn-primary" id="btn${i}">Comments</button>`
+        + '</div>'
+     + '</div>'
+    + '</div>';
+  }
+}
 
+async function poke() {
+  const counter = 7;
+  for (let i = 1; i < counter; i += 1) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+      .then((response) => response.json())
+      .then((data) => creaPoke([data]));
+  }
+}
 poke();
