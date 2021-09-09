63390ee7677bd49fde664daac3456816fc66705d
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-09-09 22:20:30 +0500

    Create basicc stucture for comments popup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 import './styles.css';
+import { popup, pokeComment } from './comments.js';
 
 const divDad = document.querySelector('.row1');
 function creaPoke(pokemon) {
   const newValues = pokemon;
-  for (let i = 0; i < newValues.length; i += 1) {
-    divDad.innerHTML += '<div class="col-4" >'
-    + '<div class="card">'
-      + `<img src="${newValues[i].sprites.front_default}" class="card-img-top" alt="image" />`
-       + '<div class="card-body">'
-          + `<h5 class="card-title">${newValues[i].name}</h5>`
-          + '<p class="card-text"></p>'
-          + `<button class="btn btn-primary" id="btn${i}">Comments</button>`
-        + '</div>'
-     + '</div>'
-    + '</div>';
-  }
+  newValues.forEach((element) => {
+    divDad.innerHTML
+      += '<div class="col-4" >'
+      + '<div class="card">'
+      + `<img src="${element.sprites.front_default}" class="card-img-top" alt="image" />`
+      + '<div class="card-body">'
+      + `<h5 class="card-title">${element.name}</h5>`
+      + '<div class="flex text-div">'
+      + '<p class="card-text text">&#9825;</p>'
+      + '<p class="likes">5</p>'
+      + '</div>'
+      + `<button class="btn btn-primary" id="${element.id}">Comments</button>`
+      + '</div>'
+      + '</div>'
+      + '</div>';
+  });
 }
 
 async function poke() {
-  const counter = 7;
-  for (let i = 1; i < counter; i += 1) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+  const counter = 34;
+  for (let i = 25; i < counter; i += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
       .then((response) => response.json())
       .then((data) => creaPoke([data]));
   }
 }
 poke();
+popup();
+pokeComment();
